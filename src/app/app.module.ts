2c715f8a66e3333197e058698e5a01b5698bffc7
Lucas Felix <lucacfelix@gmail.com>
2024-03-08 13:17:24 -0300

    configurando o module do componente e criando criando a lógica na class

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormCalc6Component } from './form-calc6/form-calc6.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { FormCalc6Component } from './form-calc6/form-calc6.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/form-calc6/form-calc6.component.ts
+++ src/app/form-calc6/form-calc6.component.ts
@@ -7,4 +7,52 @@ import { Component } from '@angular/core';
 })
 export class FormCalc6Component {
 
+  // Imput de Dados
+  num1: number = 0;
+  num2: number = 0;
+  
+  // Operadores Aritméticos
+  
+  sum: number = 0;
+  subtraction: number = 0;
+  multiplication: number = 0;
+  division: number = 0;
+  exponentiation: number = 0;
+  radication: number = 0;
+  
+  // Saida
+
+  resultado: number = 0;
+
+  // Métodos para Cálcular as operações
+
+  calcularSum(){
+    this.resultado = this.num1 + this.num2;
+  }
+
+  calcularSubtraction(){
+    this.resultado = this.num1 - this.num2;
+  }
+
+  calcularMultiplication(){
+    this.resultado = this.num1 * this.num2;
+  }
+
+  calcularDivision(){
+    if (this.num2 !== 0) {
+      this.resultado = this.num1 / this.num2;
+  } else {
+      // Tratamento de divisão por zero
+      this.resultado = NaN;
+  }
+  }
+  calcularExponentiation() {
+    this.resultado = Math.pow(this.num1, this.num2);
+}
+
+calcularRadicacao() {
+    this.resultado = Math.pow(this.num1, 1 / this.num2);
+}
+
+
 }
